d29c2016b91fa1d67225cee40558e34e7c9962bb
Surandan <110124420+Surandan@users.noreply.github.com>
2023-06-09 23:07:55 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,8 @@ package com.driver;
 public class Main {
   public static void main(String args[]) {
     RWonly obj1 = new RWonly();
+    obj1.name = "Surandan";
+    System.out.println(obj1.name);
   }
     
 }
