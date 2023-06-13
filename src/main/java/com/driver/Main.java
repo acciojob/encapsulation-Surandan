006a5350ac8f6c6a57015a621675df0ca131de7e
Surandan <110124420+Surandan@users.noreply.github.com>
2023-06-13 17:55:51 +0530

    Added-my-solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,11 @@ package com.driver;
 
 public class Main {
   public static void main(String args[]) {
-    RWonly obj1 = new RWonly();
-    obj1.name = "Surandan";
-    System.out.println(obj1.name);
+    RWOnly obj1 = new RWOnly();
+//    obj1.name = "Surandan";       // gave an error cause name is private
+//    System.out.println(obj1.name);
+    obj1.setName("Surandan");
+    System.out.println(obj1.getName());
   }
     
 }
